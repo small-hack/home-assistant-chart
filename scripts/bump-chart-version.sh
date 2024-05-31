4956b18b193b8ef37cad1daa8033e5147f9b996a
jessebot <jessebot@linux.com>
2024-05-31 09:42:11 +0200

    make bump script also handle generating helm-docs

--- scripts/bump-chart-version.sh
+++ scripts/bump-chart-version.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+HELM_DOCS_VERSION="1.13.1"
 
 set -euo pipefail
 
@@ -26,5 +27,20 @@ else
   patch=$(( patch + 1 ))
 fi
 
-echo "Bumping version for $parent_dir from $version to $major.$minor.$patch"
+echo -e "\nBumping version for $parent_dir from $version to $major.$minor.$patch"
 sed -i "s/^version:.*/version: ${major}.${minor}.${patch}/g" "charts/${parent_dir}/Chart.yaml"
+
+echo -e "\ndownloading helm-docs archive"
+curl -LO https://github.com/norwoodj/helm-docs/releases/download/v${HELM_DOCS_VERSION}/helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz
+
+echo -e "\nextracting helm-docs from archive"
+tar -xvf helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz helm-docs
+rm helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz
+
+echo -e "\nrunning helm-docs binary"
+./helm-docs
+
+echo -e "\nremoving helm-docs binary"
+rm helm-docs
+
+echo -e "\ndone"
